76b3ff76f334c676b97c5e0431043771ec35ede6
Jane Vorobjeva <jane.vorobjeva@gmail.com>
2014-03-06 17:24:32 +0400

    main: organizations list: member avatars fixed - empty avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def js_void
     return "javascript:void(0);"
   end
+
+  def user_avatar(member, size = :small)
+     if member.avatar_id
+       image_tag member.avatar_url({ size: size })
+     else
+       image_tag "avatar_#{size}.png"
+     end
+  end
 end
