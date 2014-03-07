9ca4c5c4901f8e0d0caa81a5454ca5cf69fcbb75
Jane Vorobjeva <jane.vorobjeva@gmail.com>
2014-03-07 16:46:30 +0400

    user activity page: tabs

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,14 @@
 class MembersController < ApplicationController
   def show
+    @current_member = trello_user.client.find(:member, params[:id])
+
+    @members = []
+    followers = system_user.followers
+    followers.each do |member_id|
+      @members << trello_user.client.find(:member, member_id)
+    end
+    @members.sort_by! &:full_name
+    @members = [@current_member] + @members unless followers.include? @current_member.id
   end
   
   def follow
