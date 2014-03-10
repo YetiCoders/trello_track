63cbe8b963b4ebb422bb906dd266eca20950e06c
Jane Vorobjeva <jane.vorobjeva@gmail.com>
2014-03-10 16:20:54 +0400

    user activities: some actions

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -3,60 +3,98 @@ module MembersHelper
   def action_describe(a)
     opts =
       case a[:type].to_sym
-      when :addAttachmentToCard;
-      when :addChecklistToCard;
-      when :addMemberToBoard;
-      when :addMemberToCard
-        { card: a[:data]["card"]["name"],
-          user: a[:data]["card"]["id"] }
-      when :addMemberToOrganization;
-      when :addToOrganizationBoard;
-      when :commentCard;
-      when :copyCommentCard;
-      when :convertToCardFromCheckItem;
-      when :copyBoard;
-      when :createBoard;
-      when :createCard;
-      when :copyCard;
-      when :createList;
-      when :createOrganization
-        { orgname: a[:data]["organization"]["name"] }
-      when :deleteAttachmentFromCard;
-      when :deleteBoardInvitation;
-      when :deleteCard;
-      when :deleteOrganizationInvitation;
-      when :disablePowerUp;
-      when :emailCard;
-      when :enablePowerUp;
-      when :makeAdminOfBoard;
-      when :makeNormalMemberOfBoard;
-      when :makeNormalMemberOfOrganization;
-      when :makeObserverOfBoard;
-      when :memberJoinedTrello;
-      when :moveCardFromBoard;
-      when :moveListFromBoard;
-      when :moveCardToBoard;
-      when :moveListToBoard;
-      when :removeAdminFromBoard;
-      when :removeAdminFromOrganization;
-      when :removeChecklistFromCard;
-      when :removeFromOrganizationBoard;
-      when :removeMemberFromCard;
-      when :unconfirmedBoardInvitation;
-      when :unconfirmedOrganizationInvitation;
-      when :updateBoard;
-      when :updateCard;
-      when :updateCheckItemStateOnCard;
-      when :updateChecklist;
-      when :updateList;
-      when :updateMember;
-      when :updateOrganization;
-      when :updateCard;
-      when :updateList;
+        when :addAttachmentToCard;
+        when :addChecklistToCard
+          {
+            card: a[:data]["card"]["name"],
+            checklist: a[:data]["checklist"]["name"]
+          }
+        when :addMemberToBoard;
+        when :addMemberToCard
+          {
+            card: a[:data]["card"]["name"],
+            user: a[:data]["card"]["id"]
+          }
+        when :addMemberToOrganization;
+        when :addToOrganizationBoard;
+        when :commentCard;
+        when :copyCommentCard;
+        when :convertToCardFromCheckItem;
+        when :copyBoard;
+        when :createBoard;
+        when :createCard
+          {
+            card: a[:data]["card"]["name"],
+            list: a[:data]["list"]["name"],
+          }
+        when :copyCard;
+        when :createList;
+        when :createOrganization
+          { orgname: a[:data]["organization"]["name"] }
+        when :deleteAttachmentFromCard;
+        when :deleteBoardInvitation;
+        when :deleteCard;
+        when :deleteOrganizationInvitation;
+        when :disablePowerUp;
+        when :emailCard;
+        when :enablePowerUp;
+        when :makeAdminOfBoard;
+        when :makeNormalMemberOfBoard;
+        when :makeNormalMemberOfOrganization;
+        when :makeObserverOfBoard;
+        when :memberJoinedTrello;
+        when :moveCardFromBoard;
+        when :moveListFromBoard;
+        when :moveCardToBoard;
+        when :moveListToBoard;
+        when :removeAdminFromBoard;
+        when :removeAdminFromOrganization;
+        when :removeChecklistFromCard;
+        when :removeFromOrganizationBoard;
+        when :removeMemberFromCard
+          {
+            card: a[:data]["card"]["name"],
+            user: a[:data]["card"]["id"]
+          }
+        when :unconfirmedBoardInvitation;
+        when :unconfirmedOrganizationInvitation;
+        when :updateBoard;
+        when :updateCard
+          if a[:data]["listAfter"]
+            {
+              card: a[:data]["card"]["name"],
+              list_after: a[:data]["listAfter"]["name"],
+              list_before: a[:data]["listBefore"]["name"],
+              type: "moved"
+            }
+          else
+            {
+              card: a[:data]["card"]["name"],
+              type: "updated"
+            }
+          end
+        when :updateCheckItemStateOnCard
+          {
+            card: a[:data]["card"]["name"],
+            state: a[:data]["checkItem"]["state"] + "d",
+            check_item: a[:data]["checkItem"]["name"]
+          }
+        when :updateChecklist
+          {
+            checklist: a[:data]["checklist"]["name"],
+            checklist_old: a[:data]["old"]["name"]
+          }
+        when :updateList;
+        when :updateMember;
+        when :updateOrganization;
+        when :updateCard;
+        when :updateList;
       end
 
     if opts.present?
-      t("members.activity.types.#{a[:type]}", wrap(opts))
+      Rails.logger.debug opts.inspect
+      Rails.logger.debug "members.activity.types.#{a[:type]}#{opts[:type] ? "_#{opts[:type]}" : ""}"
+      t("members.activity.types.#{a[:type]}#{opts[:type] ? "_#{opts[:type]}" : ""}", wrap(opts))
     else
       a.inspect
     end
