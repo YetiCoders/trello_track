f28baabb1f66e67c3dbeb4a2cc23557288571658
Alexander Oryol <eagle.alex@gmail.com>
2014-03-12 14:29:03 +0400

    Profile Trello API calls in development

--- config/initializers/trello.rb
+++ config/initializers/trello.rb
@@ -1,1 +1,15 @@
 require 'trello'
+
+if Rails.env.development?
+  module Trello
+    class Client
+       def invoke_verb_with_bench(*args)
+         start = Time.now
+         invoke_verb_without_bench(*args)
+        ensure
+          Rails.logger.info "==== #{args[0].to_s.upcase} #{args[1]} (#{((Time.now - start).to_f * 1000.0).to_i} msec)"
+        end
+        alias_method_chain :invoke_verb, :bench
+    end
+  end
+end
