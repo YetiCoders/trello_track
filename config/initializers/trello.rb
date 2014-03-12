f1dfc1f8ebd1fba3d62522cddcd23c8834be4adf
Alexander Oryol <eagle.alex@gmail.com>
2014-03-12 18:13:05 +0400

    pretty logging trello api calls

--- config/initializers/trello.rb
+++ config/initializers/trello.rb
@@ -7,7 +7,7 @@ if Rails.env.development?
         start = Time.now
         invoke_verb_without_bench(*args)
       ensure
-        Rails.logger.info "==== #{args[0].to_s.upcase} #{args[1]} (#{((Time.now - start).to_f * 1000.0).to_i} msec)"
+        Rails.logger.info "[trello] #{args[0].to_s.upcase} #{args[1]} (#{((Time.now - start).to_f * 1000.0).to_i} msec)"
       end
       alias_method_chain :invoke_verb, :bench
     end
