14a43f43b8437943d7daf7f81a1e18d05616fc06
Jane Vorobjeva <jane.vorobjeva@gmail.com>
2014-03-19 15:56:34 +0400

    member controller: code fixed

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,14 +1,14 @@
 class MembersController < ApplicationController
   before_action :current_member, only: [:show, :activities, :cards]
+  helper_method :organization
 
   def show
-    @organization = @current_member.organizations.select{|org| org.id == params[:organization_id]}.first
-    redirect_to main_url and return if @organization.nil?
+    redirect_to main_url and return if organization.nil?
 
     if request.xhr?
       render js: js_html("#tab_content", partial: "tab")
     else
-      @tab_members = @organization.members({ fields: ["fullName", "username", "avatarHash"] }).sort_by(&:full_name)
+      @tab_members = organization.members({ fields: ["fullName", "username", "avatarHash"] }).sort_by(&:full_name)
       @followers = system_user.followers
     end
   end
@@ -23,13 +23,11 @@ class MembersController < ApplicationController
       # if idMember exists there is member hash
       # if idMemberAdded exists there isn't member hash so we must get it
       member_id = action.data["idMemberAdded"]
-      next if member_id.nil?
+      next if member_id.nil? || @members.has_key?(member_id)
 
-      unless @members.has_key?(member_id)
-        @members[member_id] = nil
-        threads << Thread.new(member_id) do |fetch_id|
-          @members[fetch_id] = fetch_member(fetch_id)
-        end
+      @members[member_id] = nil
+      threads << Thread.new(member_id) do |fetch_id|
+        @members[fetch_id] = fetch_member(fetch_id)
       end
     end
     threads.each {|thr| thr.join }
@@ -45,16 +43,13 @@ class MembersController < ApplicationController
 
     threads = []
     for card in @cards
-      unless @card_boards.has_key?(card.board_id)
-        threads << Thread.new(card) do |card_to_fetch|
-          @card_boards[card_to_fetch.board_id] = card_to_fetch.board
-        end
-      end
-      unless @card_lists.has_key?(card.list_id)
-        threads << Thread.new(card) do |card_to_fetch|
-          @card_lists[card_to_fetch.list_id] = card_to_fetch.list
-        end
-      end
+      threads << Thread.new(card) do |card_to_fetch|
+        @card_boards[card_to_fetch.board_id] = card_to_fetch.board
+      end unless @card_boards.has_key?(card.board_id)
+
+      threads << Thread.new(card) do |card_to_fetch|
+        @card_lists[card_to_fetch.list_id] = card_to_fetch.list
+      end unless @card_lists.has_key?(card.list_id)
     end
     threads.each {|thr| thr.join }
 
@@ -76,5 +71,9 @@ class MembersController < ApplicationController
   def current_member
     @current_member ||= fetch_member(params[:id])
     redirect_to main_url if @current_member.nil?
+  end
+
+  def organization
+    @organization ||= @current_member.organizations.select{|org| org.id == params[:organization_id]}.first
   end
 end
